1d888e00d4f3cd20a7ba2c33a90bf4eb5c619f16
Daalma7 <davidvillarmartos130499@gmail.com>
2022-02-13 20:02:31 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score
+from tree import DecisionTreeClassifier
+
+
+clf = DecisionTreeClassifier(random_state=0)
+iris = load_iris()
+print(cross_val_score(clf, iris.data, iris.target, cv=10))
